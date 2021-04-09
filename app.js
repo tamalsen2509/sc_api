73961c1b69e196c2e416526257fdbb24dfb5d978
tamal.sen <tamal.sen@hotmail.com>
2021-04-09 20:10:26 +0530

    index page included

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let express = require('express');
-
-
+let cors = require ('cors');
 
 let app = express();
 
@@ -10,8 +9,20 @@ app.use(express.json());
 // db middleware
 require('./db/connection').db()
 
+// static file path
+app.use(express.static('public'))
+
+// enable cors 
+app.use(cors({optionsSuccessStatus:200}))
+
+// index route
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+// active api endpoints
 app.use('/api/social/user', require('./routes/user'));
-app.use('/api/social/post', require('./routes/posts'));
+app.use('/api/social/post', require('./routes/posts')); // in progress
 
 
 
